49554346ae1d75107c7f2a858d4a4524918042d3
alfanherya <alfanherya@gmail.com>
2020-06-05 22:57:53 +0700

    recovery

--- spring-jms-2/src/main/java/com/herya/sender/HelloSender.java
+++ spring-jms-2/src/main/java/com/herya/sender/HelloSender.java
@@ -1,4 +1,30 @@
 package com.herya.sender;
 
+import com.herya.config.JmsConfig;
+import com.herya.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
 public class HelloSender {
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+        System.out.println("i'm sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello world")
+                .build();
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("message sent!");
+    }
 }
